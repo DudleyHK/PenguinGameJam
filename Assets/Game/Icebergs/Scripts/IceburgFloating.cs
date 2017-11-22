1f8a3c8eefa1760d42bc2dc05dec75fb41e2633f
Alexander Pagliarini <Alexander2.Pagliarini@live.uwe.ac.uk>
2017-11-22 19:27:44 +0000

    skraaa

--- Assets/Game/Icebergs/Scripts/IceburgFloating.cs
+++ Assets/Game/Icebergs/Scripts/IceburgFloating.cs
@@ -56,7 +56,7 @@ public class IceburgFloating : MonoBehaviour {
                 && 0 + angleOfResistance > this.transform.eulerAngles.z)
             {
                 Debug.Log(transform.eulerAngles.z);
-                angularVelocity = Mathf.Sin( Time.time * 2) * 10;
+                angularVelocity = 
                 //angularVelocity *= slowDownSpeed * Time.deltaTime;
             }
 
