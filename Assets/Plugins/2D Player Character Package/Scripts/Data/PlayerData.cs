bdae991d8761c6d444f90589690f48d11f2af75b
Alexander Pagliarini <Alexander2.Pagliarini@live.uwe.ac.uk>
2017-11-22 16:25:26 +0000

    fixed player data

--- Assets/Plugins/2D Player Character Package/Scripts/Data/PlayerData.cs
+++ Assets/Plugins/2D Player Character Package/Scripts/Data/PlayerData.cs
@@ -2,26 +2,84 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public enum PlayerState
+public class PlayerData : MonoBehaviour
 {
-    NONE = 0,
-    GROUNDED = 1,
-    SWIMMING = 2,
-    DEAD = 3,
-}
+    public enum PlayerStates
+    {
+        NONE = 0,
+        GROUNDED = 1,
+        SWIMMING = 2,
+        DEAD = 3,
+    }
 
-public struct PlayerData
-{
-    public PlayerData(PlayerState _PlayerState, float _OxygenLevel, float _HungerLevel, float _Mass)
+    public PlayerStates playerState;
+    public float hungerLevel;
+    public float oxygenLevel;
+    public float mass;
+
+    /*public PlayerData(PlayerState _PlayerState, float _OxygenLevel, float _HungerLevel, float _Mass)
     {
         playerState = _PlayerState;
         oxygenLevel = _OxygenLevel;
         hungerLevel = _HungerLevel;
         mass = _Mass;
+    }*/
+
+    void Start()
+    {
+        playerState = PlayerStates.NONE;
+        hungerLevel = 0.0f;
+        oxygenLevel = 0.0f;
+        mass = 0.0f;
     }
 
-    public PlayerState playerState { get; set; }
-    public float hungerLevel { get; set; }
-    public float oxygenLevel { get; set; }
-    public float mass { get; set; }
+    public PlayerStates PlayerState
+    {
+        get
+        {
+            return playerState;
+        }
+        set
+        {
+            playerState = value;
+        }
+    }
+
+    public float HungerLevel
+    {
+        get
+        {
+            return hungerLevel;
+        }
+        set
+        {
+            hungerLevel = value;
+        }
+    }
+
+    public float OxygenLevel
+    {
+        get
+        {
+            return oxygenLevel;
+        }
+        set
+        {
+            oxygenLevel = value;
+        }
+    }
+
+    public float Mass
+    {
+        get
+        {
+            return mass;
+        }
+        set
+        {
+            mass = value;
+        }
+    }
+
+
 }
