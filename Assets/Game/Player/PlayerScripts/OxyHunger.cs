638393fc92e7e43ff58f0c734cacd660842de0a0
Gundars <thesatisuk@gmail.com>
2017-11-23 19:46:39 +0000

    YoungBanana
    Added the oxygen tank.. need to make it kill you hehe.. but now looks pretty pretty good..

--- Assets/Game/Player/PlayerScripts/OxyHunger.cs
+++ Assets/Game/Player/PlayerScripts/OxyHunger.cs
@@ -4,36 +4,50 @@ using UnityEngine;
 
 public class OxyHunger : MonoBehaviour {
 
-    float oxygen = 5, maxOxygen = 5;
+    public float oxygen = 10, maxOxygen = 10;
     float hunger = 10, maxHunger = 10;
 
     public bool InWater;
-    public GameObject player;
 
-    Rect OxygenRect;
-    Texture2D OxygenTex;
+    GameObject player;
+    public GameObject oxygenSprite;
 
-    Rect HungerRect;
-    Texture2D HungerTex;
+    public Sprite full;
+    public Sprite full9_10;
+    public Sprite full8_10;
+    public Sprite full7_10;
+    public Sprite full6_10;
+    public Sprite full5_10;
+    public Sprite full4_10;
+    public Sprite full3_10;
+    public Sprite full2_10;
+    public Sprite full1_10;
+    public Sprite full0_10;
+    //Rect OxygenRect;
+    //Texture2D OxygenTex;
 
-	// Use this for initialization
-	void Start () {
+    //Rect HungerRect;
+    //Texture2D HungerTex;
+
+    // Use this for initialization
+    void Start () {
         InWater = false;
+        oxygenSprite.SetActive(false);
         player = GameObject.FindGameObjectWithTag("Player");
+        //oxygenSprite = GameObject.Find("OxygenPrefab");
 
-		OxygenRect = new Rect(player.transform.position.x + 20, player.transform.position.y - 50, 5.0f, 5.0f);
-        OxygenTex = new Texture2D(1, 1);
-        OxygenTex.SetPixel(0, 0, Color.white);
-        OxygenTex.Apply();
-
-        HungerRect = new Rect(player.transform.position.x + 20, player.transform.position.y - 60, 5.0f, 5.0f);
-        HungerTex = new Texture2D(1, 1);
-        HungerTex.SetPixel(0, 0, Color.red);
-        HungerTex.Apply();
+        //full = Resources.Load("Oxygen") as Sprite;
+        //halffull = Resources.Load("Oxygen2_4") as Sprite;
+        //half = Resources.Load("Oxygen2_4") as Sprite;
+        //halfempty = Resources.Load("Oxygen1_4") as Sprite;
     }
 	
 	// Update is called once per frame
 	void Update () {
+
+        oxygenSprite.transform.position = player.transform.position - new Vector3(12.0f, -6.0f, 0.0f);
+        checkOxygenLevel();
+
         if (hunger != 0)
         {
             if (hunger < 0)
@@ -45,16 +59,22 @@ public class OxyHunger : MonoBehaviour {
 
             if (InWater)
             {
-                oxygen -= Time.deltaTime;
+                oxygen -= Time.deltaTime / 2;
+                oxygenSprite.SetActive(true);
                 if (oxygen < 0)
                 {
                     oxygen = 0;
+                    //oxygenSprite.SetActive(false);
                     //Debug.Log("Drowning!!!");
                 }
             }
             else if (!InWater && oxygen < maxOxygen)
             {
-                oxygen += Time.deltaTime * 2.0f;
+                oxygen += Time.deltaTime * 4.0f;
+                if (oxygen >= maxOxygen)
+                {
+                    oxygenSprite.SetActive(false);
+                }
             }
         }
 	}
@@ -70,7 +90,7 @@ public class OxyHunger : MonoBehaviour {
         if (other.tag == "Fish")
         {
             Destroy(other.gameObject);
-            hunger += 0.5f;
+            hunger += 0.2f;
             Debug.Log("Eaten Fish!");
         }
     }
@@ -86,15 +106,73 @@ public class OxyHunger : MonoBehaviour {
 
     void OnGUI()
     {
-        float ratioO = oxygen / maxOxygen;
-        float rectWidthO = ratioO * Screen.width / 5;
-        OxygenRect.width = rectWidthO;
+        //float ratioO = oxygen / maxOxygen;
+        //float rectWidthO = ratioO * Screen.width / 5;
+        //OxygenRect.width = rectWidthO;
 
-        float ratioH =  hunger / maxHunger;
-        float rectWidthH = ratioH * Screen.width / 5;
-        HungerRect.width = rectWidthH;
+        //float ratioH =  hunger / maxHunger;
+        //float rectWidthH = ratioH * Screen.width / 5;
+        //HungerRect.width = rectWidthH;
 
-        GUI.DrawTexture(OxygenRect, OxygenTex);
-        GUI.DrawTexture(HungerRect, HungerTex);
+        //GUI.DrawTexture(OxygenRect, OxygenTex);
+        //GUI.DrawTexture(HungerRect, HungerTex);
+    }
+
+    void checkOxygenLevel()
+    {
+        if (oxygen <= 0.0f)
+        {
+            oxygenSprite.GetComponent<SpriteRenderer>().sprite = full0_10;
+        }
+        else if (oxygen <= 1.0f)
+        {
+            oxygenSprite.GetComponent<SpriteRenderer>().sprite = full1_10;
+        }
+        else if (oxygen <= 2.0f)
+        {
+            oxygenSprite.GetComponent<SpriteRenderer>().sprite = full2_10;
+        }
+        else if (oxygen <= 3.0f)
+        {
+            oxygenSprite.GetComponent<SpriteRenderer>().sprite = full3_10;
+        }
+        else if (oxygen <= 4.0f)
+        {
+            oxygenSprite.GetComponent<SpriteRenderer>().sprite = full4_10;
+        }
+        else if (oxygen <= 5.0f)
+        {
+            oxygenSprite.GetComponent<SpriteRenderer>().sprite = full5_10;
+        }
+        else if (oxygen <= 6.0f)
+        {
+            oxygenSprite.GetComponent<SpriteRenderer>().sprite = full6_10;
+        }
+        else if (oxygen <= 7.0f)
+        {
+            oxygenSprite.GetComponent<SpriteRenderer>().sprite = full7_10;
+        }
+        else if (oxygen <= 8.0f)
+        {
+            oxygenSprite.GetComponent<SpriteRenderer>().sprite = full8_10;
+        }
+        else if (oxygen <= 9.0f)
+        {
+            oxygenSprite.GetComponent<SpriteRenderer>().sprite = full9_10;
+        }
+        else
+        {
+            oxygenSprite.GetComponent<SpriteRenderer>().sprite = full;
+        }
     }
 }
+
+//OxygenRect = new Rect(player.transform.position.x, player.transform.position.y, 5.0f, 5.0f);
+//      OxygenTex = new Texture2D(1, 1);
+//      OxygenTex.SetPixel(0, 0, Color.white);
+//      OxygenTex.Apply();
+
+//      HungerRect = new Rect(player.transform.position.x, player.transform.position.y, 5.0f, 5.0f);
+//      HungerTex = new Texture2D(1, 1);
+//      HungerTex.SetPixel(0, 0, Color.red);
+//      HungerTex.Apply();
