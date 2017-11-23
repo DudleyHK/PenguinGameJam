84c5f67dc0f2108e489855de633a70fc68a34158
jackalyn m <kaos@thegame.com>
2017-11-23 16:40:46 +0000

    Menu manager n tags stuff for ui and menus

---
+++ Assets/Game/Menu/GameState.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum GameState
+{
+	NONE = 0,
+	START = 1,
+	LOBBY = 2,
+	PLAY = 3,
+	PAUSE = 4,
+	GAMEOVER = 5,
+	OPTIONS = 6,
+	IDLE = 7
+}
+
---
+++ Assets/Game/Menu/MenuMan.cs
@@ -0,0 +1,96 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class MenuMan : MonoBehaviour
+{
+
+
+	private GameState gameState;
+	private GameState nextState;
+
+	// Use this for initialization
+	void Start () {
+		gameState = GameState.START;
+		nextState = GameState.NONE;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		checkState ();
+		loadChilds ();
+	}
+
+	public GameState currentState
+	{
+		get
+		{
+			return gameState;
+		}
+		set
+		{
+			gameState = value;
+		}
+	}
+
+	void checkState()
+	{
+		//more stuff here
+		//might be just useless though honestly
+		if (nextState != GameState.NONE)
+		{
+			gameState = nextState;
+		}
+	}
+
+	void loadChilds()
+	{
+		bool show;
+		foreach (SpriteRenderer i in this.GetComponentsInChildren<SpriteRenderer>())
+		{
+			show = false;
+			switch (gameState) {
+			case GameState.START: 
+				if (i.gameObject.tag == "Start") {
+					show = true;
+				}
+				break;
+			case GameState.PAUSE:
+				if (i.gameObject.tag == "Pause" || i.gameObject.tag == "UI") {
+					show = true;
+				}
+				break;
+			case GameState.GAMEOVER:
+				if (i.gameObject.tag == "GameOver") {
+					show = true;
+				}
+				break;
+			case GameState.PLAY:
+				if (i.gameObject.tag == "UI") {
+					show = true;
+				}
+				break;
+					}
+
+			/*switch (i.tag) 
+			{
+			case "Options":
+				break;
+			case "Lobby":
+				break;
+			case "Game Over":
+				break;
+			case "Start":
+				break;
+			case "Pause":
+			case "UI": 
+
+
+				break;
+			}*/
+			i.enabled = show;
+		}
+
+	}
+}
