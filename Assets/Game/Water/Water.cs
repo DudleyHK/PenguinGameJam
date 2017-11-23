7bc311ca977b08256a77a98e0dcc3060586d651b
Dudley Dawes <dudley2.dawes@live.uwe.ac.uk>
2017-11-23 12:52:58 +0000

    Static water data class added

---
+++ Assets/Game/Water/Water.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Water : MonoBehaviour
+{
+    public static Vector2 waterOrigin;
+
+
+	private void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	private void Update ()
+    {
+        var waterCentreX = transform.position.x + (transform.lossyScale.x / 2f);
+        var waterCentreY = transform.position.y - (transform.lossyScale.y / 2f);
+        waterOrigin = new Vector2(waterCentreX, waterCentreY);
+
+    }
+}
