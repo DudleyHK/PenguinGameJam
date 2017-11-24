88d188e52104bf6d579a471bc5ff13e2729c75f8
Alexander Pagliarini <Alexander2.Pagliarini@live.uwe.ac.uk>
2017-11-24 23:54:25 +0000

    Menu Progress

---
+++ Assets/Game/Menu/PlayerAdding.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAdding : MonoBehaviour {
+
+    public int thisIndex;
+
+    public bool ControllerActive;
+
+    public SpriteRenderer penguin;
+
+
+    // Use this for initialization
+    void Start () {
+        penguin.enabled = false;	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (ControllerActive)
+        {
+            if (Input.GetButtonUp("Controller "+ thisIndex +" - B"))
+            {
+                ControllerActive = false;
+                penguin.enabled = false;
+            }
+
+        }
+
+        if (!ControllerActive)
+        {
+            if (Input.GetButtonUp("Controller " + thisIndex + " - A"))
+            {
+                ControllerActive = true;
+                penguin.enabled = true;
+            }
+
+        }
+
+
+    }
+}
