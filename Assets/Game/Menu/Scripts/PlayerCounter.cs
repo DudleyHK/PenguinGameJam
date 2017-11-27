ccdadc003d5a10c9a8476cfb6669c0b599a0fa03
Alexander Pagliarini <Alexander2.Pagliarini@live.uwe.ac.uk>
2017-11-27 19:29:05 +0000

    Spawning Players From Menu

---
+++ Assets/Game/Menu/Scripts/PlayerCounter.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerCounter : MonoBehaviour {
+
+    [SerializeField]
+    int numberOfActiveControllers, numberOfLockedInPlayers;
+
+    public PlayerAdding[] possiblePlayers;
+
+	// Use this for initialization
+	void Start ()
+    {
+        DontDestroyOnLoad(transform.gameObject);
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        numberOfActiveControllers   = 0;
+        numberOfLockedInPlayers     = 0;
+
+        foreach (PlayerAdding player in possiblePlayers)
+        {
+            if(player.ControllerActive)
+            {
+                numberOfActiveControllers++;
+            }
+
+            if(player.lockedIn)
+            {
+                numberOfLockedInPlayers++;
+            }
+            
+        }
+
+        if(numberOfActiveControllers == numberOfLockedInPlayers &&
+            numberOfActiveControllers > 1)
+        {
+            Debug.Log("Ready to start");
+            startGame();
+        }
+        else
+        {
+            Debug.Log("Waiting for players . . .");
+        }
+
+    }
+
+    void startGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+}
