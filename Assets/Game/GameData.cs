40bc6fbebb83a989c7fe7f5f6b1ede30984f0e6f
Mitchell Height <Mitchell2.Height@live.uwe.ac.uk>
2017-11-28 19:56:11 +0000

    Only 1 iceberg melts at once. Iceberg is randomly chosen to melt. Icebergs need to be added to the list of gameobjects in gamedata when created.

--- Assets/Game/GameData.cs
+++ Assets/Game/GameData.cs
@@ -9,19 +9,25 @@ public class GameData : MonoBehaviour {
     static int fishCap = 0;
     static int currentFishSpawned = 0;
     static List<GameObject> players;
+    static List<GameObject> icebergs;
+    static bool isIcebergMelting = false;
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start () {
         killerWhaleTimer = Random.Range(15, 60);
         currentFishSpawned = 0;
         fishCap = 100;
         bestPlayer = 0;
         players = null;
-	}
+        isIcebergMelting = false;
+    }
 	
 	// Update is called once per frame
 	void Update () {
-		
+		if(!IsIcebergMelting)
+        {
+            chooseMeltedIceberg();
+        }
 	}
 
     public static int BestPlayer()
@@ -39,6 +45,18 @@ public class GameData : MonoBehaviour {
             }
         }
         return bestPlayersIndex;
+    }
+
+    public static void chooseMeltedIceberg()
+    {
+        if(icebergs.Capacity > 0)
+        {
+            int icebergIndex = Random.Range(0, icebergs.Capacity);
+            IceburgFloating data = icebergs[icebergIndex - 1].GetComponent<IceburgFloating>();
+            isIcebergMelting = true;
+            data.Melting = true;
+        }
+        
     }
 
     public static int FishSpawnedCount
@@ -74,6 +92,18 @@ public class GameData : MonoBehaviour {
         set
         {
             killerWhaleTimer = value;
+        }
+    }
+
+    public static bool IsIcebergMelting
+    {
+        get
+        {
+            return isIcebergMelting;
+        }
+        set
+        {
+            isIcebergMelting = value;
         }
     }
 }
